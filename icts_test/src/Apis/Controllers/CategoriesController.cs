9559db1f4d46f4357a9c844e5093d0b23043301d
Luiz Felipe <raziel_libertino@hotmail.com>
2023-06-22 08:28:11 -0400

    Parseando para rest controller category

--- icts_test/src/Apis/Controllers/CategoriesController.cs
+++ icts_test/src/Apis/Controllers/CategoriesController.cs
@@ -30,52 +30,74 @@ namespace icts_test.WebAPIs.Controllers
         }
 
         [HttpPost("add")]
-        public async Task<List<Notifies>> Add([FromBody] CategoryViewModel category)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Add([FromBody] CategoryViewModel category)
         {
             var categoryMap = _mapper.Map<Category>(category);
 
             await _serviceCategory.Add(categoryMap);
 
-            return categoryMap.Notitycoes;
+            if (categoryMap.Notitycoes.Any()) return BadRequest(categoryMap.Notitycoes);
+
+            return StatusCode(StatusCodes.Status201Created);
         }
 
         [HttpPut("update")]
-        public async Task<List<Notifies>> Update([FromBody] CategoryViewModel category)
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update([FromBody] CategoryViewModel category)
         {
+            var categoryDB = await _category.GetEntityById(category.Id);
+
+            if (categoryDB == null) return NotFound("Categoria inexistente.");
+
             var categoryMap = _mapper.Map<Category>(category);
+            
 
             await _serviceCategory.Update(categoryMap);
 
-            return categoryMap.Notitycoes;
+            if (categoryMap.Notitycoes.Any()) return BadRequest(categoryMap.Notitycoes);
+
+            return NoContent();
         }
 
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
 
             var result = await _serviceCategory.DeleteById(id);
 
-            if (!result) return BadRequest("Categoria inexistente.");
+            if (!result) return NotFound("Categoria inexistente.");
 
             return NoContent();
         }
 
         [HttpGet("{id}")]
-        public async Task<CategoryViewModel> Get([FromRoute] int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get([FromRoute] int id)
         {
             var category = await _category.GetEntityById(id);
+
+            if (category == null) return NotFound("Categoria inexistente.");
+
             var categoryMap = _mapper.Map<CategoryViewModel>(category);
 
-            return categoryMap;
+            return Ok(categoryMap);
         }
 
         [HttpGet]
-        public async Task<List<CategoryViewModel>> List()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> List()
         {
             var categories = await _category.List();
             var categoriesMap = _mapper.Map<List<CategoryViewModel>>(categories);
 
-            return categoriesMap;
+            return Ok(categoriesMap);
         }
     }
 }
